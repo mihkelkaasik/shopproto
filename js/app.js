33301c8662e0525b7f1245e01566a886626646e0
mihkelkaasik <mihkelk@ut.ee>
2016-04-22 12:53:18 +0300

    njii

--- js/app.js
+++ js/app.js
@@ -15,21 +15,21 @@ phonecatApp.config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/home/', {
-        templateUrl: '/app/partials/phone-list.html',
+        templateUrl: '/partials/phone-list.html',
         controller: 'PhoneListCtrl'
       }).
       when('/phone/:phoneId', {
-        templateUrl: '/app/partials/phone-detail.html',
+        templateUrl: '/partials/phone-detail.html',
         controller: 'PhoneDetailCtrl'
       }).
 	  when('/nigga/', {
-		  templateUrl: '/app/partials/nigga.html'
+		  templateUrl: '/partials/nigga.html'
 	  }).	  
 	  when('/products/', {
-		  templateUrl: '/app/partials/products.html'
+		  templateUrl: '/partials/products.html'
 	  }).	  
 	  when('/register/', {
-		  templateUrl: '/app/partials/register.html'
+		  templateUrl: '/partials/register.html'
 	  }).
       otherwise({
         redirectTo: '/home/'
